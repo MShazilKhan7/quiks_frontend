0bb47fb883b702056765369e4cf38ca43340f8c4
MShazilKhan7 <shazilk14@gmail.com>
2024-08-02 20:41:19 +0500

    sidebar added

--- src/app/components/sidebar/SidebarItem.jsx
+++ src/app/components/sidebar/SidebarItem.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-export default function SidebarItem(props) {
+
+export default function SidebarItem({
+  icon,
+  title,
+  index,
+  setActiveIndex,
+  isActive,
+  path,
+}) {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    setActiveIndex(index);
+    navigate(path);
+  };
   return (
     <div
-      onClick={() => {
-        navigate(props.path);
-      }}
-      className="cursor-pointer"
+      onClick={handleClick}
+      className={`cursor-pointer p-2 rounded-xl ${
+        isActive ? "bg-focused_menu_bg" : ""
+      }`}
     >
-      <p>{props.title}</p>
+      <div
+        className={`text-sm flex flex-col gap-1 justify-center items-center text-menu ${
+          isActive ? "text-focused_menu" : ""
+        }`}
+      >
+        <div className="text-[24px] font-bold">{icon}</div>
+        <p className="text-[12px]">{title}</p>
+      </div>
     </div>
   );
 }
