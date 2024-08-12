d7be2d4d79b845cf0578023cf44be8a3991b40c9
MShazilKhan7 <shazilk14@gmail.com>
2024-08-12 17:03:15 +0500

    categories scrollar issue solved.

--- src/app/components/categories/Categories.jsx
+++ src/app/components/categories/Categories.jsx
@@ -5,19 +5,17 @@ const Categories = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className="categories py-2">
-      <div className="categories flex flex-row gap-3 pb-4 overflow-x-auto md:overflow-hidden">
-        {categoires.map((item, index) => {
-          return (
-            <CategoryItem
-              {...item}
-              isActive={activeIndex === index}
-              setActiveIndex={setActiveIndex}
-              index={index}
-            />
-          );
-        })}
-      </div>
+    <div className="categories flex flex-row gap-3 pb-4">
+      {categoires.map((item, index) => {
+        return (
+          <CategoryItem
+            {...item}
+            isActive={activeIndex === index}
+            setActiveIndex={setActiveIndex}
+            index={index}
+          />
+        );
+      })}
     </div>
   );
 };
